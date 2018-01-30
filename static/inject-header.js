192e515b7d4fb8dbf205c2080e524b564f43dd88
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2018-01-31 01:26:24 +0200

    pre-alpha version

--- static/inject-header.js
+++ static/inject-header.js
@@ -1,7 +1,6 @@
-var scraperFn = XMLHttpRequest.prototype.open;
-var siteRealUrl = location.pathname.replace(/^\/(https?:\/\/?)([^/]+).*$/, '$1/$2');
-debugger
-if (scraperFn.toString().indexOf('checkNeedPrototype') === -1) {
+if (XMLHttpRequest.prototype.open.toString().indexOf('checkNeedPrototype') === -1) {
+	var scraperFn = XMLHttpRequest.prototype.open;
+	var siteRealUrl = location.pathname.replace(/^\/(https?:\/\/?)([^/]+).*$/, '$1/$2');
 	XMLHttpRequest.prototype.open = function(method, path, p) {
 		var checkNeedPrototype;
 		// var isStatic = path.endsWith('.js') || path.endsWith('.css') || path.endsWith('.jpg') || path.endsWith('.png');
@@ -15,19 +14,19 @@ if (scraperFn.toString().indexOf('checkNeedPrototype') === -1) {
 		// console.log(path);
 		scraperFn.call(this, method, `/${path}`, p);
 	};
-}
 
-window.onbeforeunload = function() {
-	return 'Are you sure you want to leave?';
-};
+	window.onbeforeunload = function() {
+		return 'Are you sure you want to leave?';
+	};
 
-window.onload = function() {
-	// var aList = document.querySelectorAll('a');
-	// aList.forEach(a => setTimeout(() => {
-	// 	if (a.href.startsWith(siteRealUrl)) {
-	// 		a.setAttribute('href', a.href.replace())
-	// 		console.log('/' + a.href);
-	// 	}
-	// }));
-	document.body.style.pointerEvents = 'auto';
+	window.onload = function() {
+		// var aList = document.querySelectorAll('a');
+		// aList.forEach(a => setTimeout(() => {
+		// 	if (a.href.startsWith(siteRealUrl)) {
+		// 		a.setAttribute('href', a.href.replace())
+		// 		console.log('/' + a.href);
+		// 	}
+		// }));
+		document.body.style.pointerEvents = 'auto';
+	}
 }
